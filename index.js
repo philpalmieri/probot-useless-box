bad2c30620e96805a8c47558c94a0f5fd847c2f6
Phil Palmieri <ppalmieri@partnercomm.net>
2018-06-18 07:48:24 -0400

    Add probot schema checker

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+const ProbotConfigCheck = require('probot-report-error');
+const schema = require('./schema');
+
 module.exports = robot => {
-    // your code here
+    const pccOptions = { 
+        title: 'ProBot Error Checker YAML is Bad',
+        config: './config.yml',
+        schema: schema,
+        body: 'Something is wrong!',
+        owner: 'philpalmieri',
+        repo: 'probot-useless-box'
+    }
+
+    robot.on('issues.reopened', async context => {
+        try {
+            const pbc = new ProbotConfigCheck(context.github, pccOptions);
+
+            if(!await pbc.validate()) {
+                // Create issue
+               await pbc.createIssue();
+            }
+            // Code was pushed to the repo, what should we do with it?
+
+            return context.github.issues.edit(context.issue({state: 'closed'}));  
+        } catch( e ) {
+           console.log('Outer Error', e.message);
+        }
+    });
 }
